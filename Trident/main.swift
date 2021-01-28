50f14f93b4baa5b514e192cc81dc261cfb85e3fa
yining wang <ywang@lcs.on.ca>
2021-01-28 09:10:07 -0500

    get one tine to success

--- Trident/main.swift
+++ Trident/main.swift
@@ -18,31 +18,36 @@ print("=======")
 // Get tine length
 print("Enter tine length:")
 print("   ", terminator: "")
-let tineLength = Int(readLine()!)!
+let t = Int(readLine()!)!
 
 // Get tine spacing
 print("Enter tine spacing:")
 print("   ", terminator: "")
-let spacing = Int(readLine()!)!
+let s = Int(readLine()!)!
 
 // OUTPUT
 
-// Produce top of trident according to length given
-for _ in 1...tineLength {
-    // Print the tines
-    for _ in 1...3 {
-        
-        // Print part of a tine
-        print("*", terminator: "")
-        
-        // Print space between tines
-        for _ in 1...spacing {
-            print(" ", terminator: "")
-        }
-        
-    }
-    // Go to next line of output
-    print("")
+//Draw one time
+for _ in 1...t {
+    print("*")
 }
 
-
+//// Produce top of trident according to length given
+//for _ in 1...tineLength {
+//    // Print the tines
+//    for _ in 1...3 {
+//
+//        // Print part of a tine
+//        print("*", terminator: "")
+//
+//        // Print space between tines
+//        for _ in 1...spacing {
+//            print(" ", terminator: "")
+//        }
+//
+//    }
+//    // Go to next line of output
+//    print("")
+//}
+//
+//
